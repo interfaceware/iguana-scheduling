1283d6661741bac143c0bf147f7fc81970afacdd
admin <eliot.muir@interfaceware.com>
2016-03-29 16:07:41 +0000

    Export from Xero 2 Server: Put in help and fleshed out example.

--- cron_-_edit_crontab-FromHTTP-qSKZ3hPjp5V58J/main.lua
+++ cron_-_edit_crontab-FromHTTP-qSKZ3hPjp5V58J/main.lua
@@ -1,10 +1,13 @@
+-- Simple example show programmatically reading and writing from the crontab file.
+
 local cron = require 'cron.edit'
 
 local Template=[[
 <html>
 <body>
 <p>
-This is the crontab for this user:
+This is the crontab for this user.  If you refresh the script it will add and remove a comment in the crontab.
+Thus this shows how one can use Iguana to programmatically alter the crontab file for the user that Iguana is running under.
 </p>
 <pre>
 #CRONTAB#
@@ -15,6 +18,15 @@ This is the crontab for this user:
 
 function main(Data)
    local Content = cron.read()
-   
+   local Comment = "# This comment was added by Iguana"
+   if not Content:find(Comment) then
+      Content = Content..Comment
+      cron.write(Content)
+   else
+      Content = Content:gsub(Comment, '')
+      cron.write(Content)
+   end
+
+   trace(Content)   
    net.http.respond{body=Template:gsub("#CRONTAB#", Content)}
 end
